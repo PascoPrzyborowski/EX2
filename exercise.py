a9b9c7e473d0ad4c5cb48322e1dfc5189bdaea32
PascoPrzyborowski <unity01@gmx.net>
2022-08-10 16:16:19 +0200

    Ex 2

--- exercise.py
+++ exercise.py
@@ -1,3 +1,55 @@
 # do exercise here
 # upload to github for portfolio
 # thank me later when your git graph is as green as the python money u gonna earn
+
+import math
+
+#Task 1
+
+a = 12 + 34 # =46
+b = 43 - 56 # = -13
+c = 4 * 4   # = 16
+d = 4 / 13  # = 0.8
+e = 12 % 5  # = 2
+f = 2 ** 4  # = 16
+g = 12.2 // 3 # = 4.0
+h = True * False # = 0
+i = 5 * (2 - 3j) # = (10-15j)
+
+
+print('12 + 34 =',a)
+print('43 - 56 =',b)
+print('4 * 4 =',c)
+print('4 / 13 =',d)
+print('12 % 5 =',e)
+print('2 ** 4 =',f)
+print('12.2 // 3 =',g)
+print('True * False =',h)
+print('5 * (2 - 3j) =',i)
+
+#Task 2
+
+
+a = max(5, -20, 1000, 958223.5, 0.678)
+print(a)
+b = max('hello','pasco')
+print(b)
+c = min('nadia','felipe','pasco','dimi','michel') 
+print(c)
+d = min('xmen','marvel','python','data')
+print(d)
+print(ord('a')) # Ascii Zeichen Nummer
+e = abs(-34.23)
+print(e)
+f = pow(3, 4)
+print(f)
+g = math.ceil(34.2) #round up
+print(g)
+h = math.floor(34.7) #round down
+print(h)
+j = max(True, False)
+print(j)
+k = min(True, False)
+print(k)
+l = abs(4 - 3j)
+print(l)
